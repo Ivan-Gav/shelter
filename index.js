6f35498d6049e402ad6356458c2e97dc52b041e4
Ivan <ivan.gavrilin@inbox.ru>
2023-04-07 00:48:25 +0300

    feat: add burger menu

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-// console.log(" Результаты самопроверки part 1: \n верстка страницы валидная +4 \n логотип в хедере состоит из текстовых элементов +1 \n страница содержит ровно один элемент <h1> +1 \n добавлен favicon +1 \n Вёрстка соответствует макету +35 \n блок <header> +5 \n блок Not only +5 \n блок About +5 \n блок Our Friends +5 \n блок Help +5 \n блок In addition +5 \n блок <footer> +5 \n Требования к css +6 \n для позиционирования элементов блока Help использована сеточная верстка (flexbox или grid) +2 \n при уменьшении масштаба страницы браузера или увеличении ширины страницы (>1280px) вёрстка размещается по центру, а не сдвигается в сторону и не растягивается по всей ширине +2 \n фоновый цвет тянется на всю ширину страницы +2 \n Интерактивность элементов +12 \n элемент About the Shelter в навигации подсвечен и неинтерактивен, остальные элементы навигации интерактивны +2 \n каждая карточка с питомцем в блоке Our Friends интерактивна при наведении на любую область этой карточки +2 \n плавная прокрутка по якорям +2 \n выполняются все ссылочные связи согласно Перечню ссылочных связей для страницы Main +2 \n выполнена интерактивность ссылок и кнопок. +2 \n обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике, не влияющее на соседние элементы +2 \n ");
+const body = document.body
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu')
 
-console.log(" Результаты самопроверки part 2: \n Вёрстка страницы Main соответствует макету при ширине экрана 1280px: +14 \n блок <header>: +2 \n блок Not only: +2 \n блок About: +2 \n блок Our Friends: +2 \n блок Help: +2 \n блок In addition: +2 \n блок <footer>: +2 \n Вёрстка страницы Main соответствует макету при ширине экрана 768px: +14 \n блок <header>: +2 \n блок Not only: +2 \n блок About: +2 \n блок Our Friends: +2 \n блок Help: +2 \n блок In addition: +2 \n блок <footer>: +2 \n Вёрстка страницы Main соответствует макету при ширине экрана 320px: +14 \n блок <header>: +2 \n блок Not only: +2 \n блок About: +2 \n блок Our Friends: +2 \n блок Help: +2 \n блок In addition: +2 \n блок <footer>: +2 \n Вёрстка страницы Pets соответствует макету при ширине экрана 1280px: +6 \n блок <header>: +2 \n блок Our Friends: +2 \n блок <footer>: +2 \n Вёрстка страницы Pets соответствует макету при ширине экрана 768px: +6 \n блок <header>: +2 \n блок Our Friends: +2 \n блок <footer>: +2 \n Вёрстка страницы Pets соответствует макету при ширине экрана 320px: +6 \n блок <header>: +2 \n блок Our Friends: +2 \n блок <footer>: +2 \n нет полосы прокрутки при ширине страницы Main от 1280рх до 768рх: +5 \n нет полосы прокрутки при ширине страницы Main от 768рх до 320рх: +5 \n нет полосы прокрутки при ширине страницы Pets от 1280рх до 768рх: +5 \n нет полосы прокрутки при ширине страницы Pets от 768рх до 320рх: +5 \n резиновая верстка на странице Main: +4 \n резиновая верстка на странице Pets: +4 \n появляется иконка бургер-меню: +4 \n верстка обеих страниц валидная: +8")
+const activateBurgerMenu = () => {
+  
+    burger.addEventListener('click', toggleBurgerMenu)
+    document.addEventListener('click', (event) => {
+        if (((event.target.tagName === 'A') 
+            || (event.target.className === 'overlay active'))
+            && (burger.className == 'burger active')) {
+            hideBurgerMenu()
+        }
+    })
+}
+
+const toggleBurgerMenu = () => {
+    addOverlay(menu).classList.toggle('active')
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+    body.classList.toggle('disable-scroll')
+}
+
+const addOverlay = (element) => {
+    element.insertAdjacentHTML('beforebegin', '<div class="overlay">')
+    element.insertAdjacentHTML('afterend', '<div>')
+    return document.querySelector('.overlay')
+}
+
+const hideBurgerMenu = () => {
+    document.querySelector('.overlay').classList.remove('active')
+    burger.classList.remove('active')
+    menu.classList.remove('active')
+    body.classList.remove('disable-scroll')
+}
+
+    activateBurgerMenu()
